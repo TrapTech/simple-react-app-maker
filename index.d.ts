a2283a425fb4e3b934e7517ab3ec27e1577489ee
mdarocha <git@mdarocha.pl>
2022-10-23 12:24:10 +0200

    Add typescript types

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly NODE_ENV: "development" | "production";
+    }
+}
+
+declare module "*.jpg" {
+    const src: string;
+    export default src;
+}
+
+declare module "*.jpeg" {
+    const src: string;
+    export default src;
+}
+
+declare module "*.png" {
+    const src: string;
+    export default src;
+}
+
+declare module "*.webp" {
+    const src: string;
+    export default src;
+}
+
+declare module "*.svg" {
+    import * as React from "react";
+
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+
+    const src: string;
+    export default src;
+}
